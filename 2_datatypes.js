50a040360411a3e170f3c97e646b4fdd8f500e71
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2024-08-02 11:38:54 +0530

    Add files via upload

---
+++ 2_datatypes.js
@@ -0,0 +1,23 @@
+console.log(3+3)
+
+console.log("Hitesh");
+
+
+let name = "tharunn"
+let age = 20
+let isLoggedIn = false
+let state;
+
+/*
+number => 2 to power 53
+big int
+string
+boolean => true/false
+null => standalone
+undefined  =>
+symbol => unique
+*/
+console.log(typeof age); 
+console.log(typeof null);
+
+console.log(typeof undefined);
