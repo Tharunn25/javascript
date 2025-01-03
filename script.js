2c1c4c485deb0f5cce84719d38cc13e91610e41b
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-01-03 23:50:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded',()=>{
          const expenseForm = document.getElementById('expense-form');
          const expenseNameInput = document.getElementById('expense-name');
          const expenseAmountInput = document.getElementById('expense-amount');
-         const expenseList = document.getElementById('total');
+         const expenseList = document.getElementById('expense-list');
          const totalAmountDisplay = document.getElementById('total-amount');
 
-         let expenses = [];
+         let expenses = JSON.parse(localStorage.getItem('expenses'))||[];
          let totalAmount = calculateTotal();
+
+         renderExpenses
 
 
          expenseForm.addEventListener('submit',(e)=>{
@@ -15,7 +17,7 @@ document.addEventListener('DOMContentLoaded',()=>{
             const name = expenseNameInput.value.trim()
             const amount = parseFloat(expenseAmountInput.value.trim());
 
-            if(name !== "" && isNaN(amount) && amount >0){
+            if(name !== "" && !isNaN(amount) && amount > 0){
               const newExpense = {
                 id: Date.now(),
                 name,
@@ -23,18 +25,33 @@ document.addEventListener('DOMContentLoaded',()=>{
               }
               expenses.push(newExpense)
               saveExpensesTolocal();
-            }
+              renderExpenses();
+              updateTotal();
+            
 
             // clear input
 
             expenseNameInput.value = "";
             expenseAmountInput.value = "";
+          }
          });
+
+
+         function renderExpenses(){
+             expenseList.innerHTML = ""
+             expenses.forEach(expense => {
+              document.createElement('li')
+              li.innerHTML = `
+              ${expense.name} - $${expense.amount}
+              <button data-id="${expense.id}">Delete</button>
+              `
+             })
+         }
 
 
          function calculateTotal(){
              
-             return expenses
+             return expenses.reduce((sum, expense)=> sum + expense.amount,0);
 
          };
 
@@ -42,13 +59,12 @@ document.addEventListener('DOMContentLoaded',()=>{
           localStorage.setItem("expenses",JSON.stringify(expenses))
          }
 
-         
+         function updateTotal(){
+              totalAmount = calculateTotal()
+              totalAmountDisplay.textContent = totalAmount.toFixed(2);
+              
+              
 
+         }
 
-
-
-
-
-
-
-});
+}); 
